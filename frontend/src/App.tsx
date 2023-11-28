1c73c7029c11a12ab982f52819bcc7ec2bdd7792
sumit chauhan <sumitchauhan49@gmail.com>
2023-11-28 09:23:43 +0000

    view more function

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { FormEvent, useState } from "react";
+import { FormEvent, useRef, useState } from "react";
 import * as api from "./api";
 import { Recipe } from "./types";
 import RecipeCard from "./components/RecipeCard";
@@ -7,22 +7,34 @@ import RecipeCard from "./components/RecipeCard";
 const App = () => {
   const [searchTerm, setSearchTerm] = useState<string>("");
   const [recipes, setRecipes] = useState<Recipe[]>([]);
+  const pageNumber = useRef(1);
 
   const handleSearchSubmit = async (event: FormEvent) => {
     event.preventDefault();
     try {
       const recipes = await api.searchRecipes(searchTerm, 1);
       setRecipes(recipes.results);
-      // pageNumber.current = 1;
+      pageNumber.current = 1;
     } catch (e) {
       console.log(e);
+    }
+  };
+
+  const handleViewMoreClick = async () => {
+    try {
+      const nextPage = pageNumber.current + 1;
+      const nextRecipes = await api.searchRecipes(searchTerm, nextPage);
+      setRecipes((prevRecipes) => [...prevRecipes, ...nextRecipes.results]);
+      pageNumber.current = nextPage;
+    } catch (error) {
+      console.error(error);
     }
   };
 
   return (
     <div>
       <form onSubmit={handleSearchSubmit}>
-      <input
+        <input
           type="text"
           required
           placeholder="Enter a search term"
@@ -32,8 +44,11 @@ const App = () => {
         <button type="submit">Submit</button>
       </form>
       {recipes.map((recipe) => (
-       <RecipeCard key={recipe.id} recipe={recipe} />
+        <RecipeCard key={recipe.id} recipe={recipe} />
       ))}
+      <button className="view-more" onClick={handleViewMoreClick}>
+        View More
+      </button>
     </div>
   );
 };
